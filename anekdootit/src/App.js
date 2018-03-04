ba89b92391cc50fca3e6f34ee1fd4a89853d19d0
Falzed <oskari_kulmala@hotmail.com>
2018-03-04 20:23:35 +0200

    5.19, 5.20, 5.21

--- anekdootit/src/App.js
+++ anekdootit/src/App.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
 class App extends React.Component {
+
+
+
+  vote = (id) => () => {
+    this.props.store.dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  addAnecdote = (event) => {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'NEW_ANECDOTE',
+      data: event.target.anecdote.value
+    })
+  }
+
   render() {
     const anecdotes = this.props.store.getState()
+
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote=>
+        {anecdotes.sort((firstAnecdote, secondAnecdote) =>
+          secondAnecdote.votes - firstAnecdote.votes
+        ).map(anecdote =>
           <div key={anecdote.id}>
             <div>
-              {anecdote.content} 
+              {anecdote.content}
             </div>
             <div>
               has {anecdote.votes}
-              <button>vote</button>
+              <button onClick={this.vote(anecdote.id)}>vote</button>
             </div>
           </div>
-        )}
+          )}
         <h2>create new</h2>
-        <form>
-          <div><input /></div>
-          <button>create</button> 
+        <form onSubmit={this.addAnecdote}>
+          <div><input type="text" name="anecdote" /></div>
+          <button>create</button>
         </form>
       </div>
     )
